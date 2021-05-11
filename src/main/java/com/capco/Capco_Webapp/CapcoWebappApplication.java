4ab0b4110dc281ddfe689686a3a0e66fe5ecfb27
mohit <mohit.gangil2012@gmail.com>
2021-05-11 19:51:21 +0530

    Maven Training purpose

---
+++ src/main/java/com/capco/Capco_Webapp/CapcoWebappApplication.java
@@ -0,0 +1,13 @@
+package com.capco.Capco_Webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapcoWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapcoWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capco/Capco_Webapp/CapcoWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capco.Capco_Webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapcoWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
